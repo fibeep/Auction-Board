b2cf713d0715d2b7de648917f608061cbb136e38
Salo <salomonco@gmail.com>
2021-07-11 13:52:20 -0500

    message contract base started

--- contracts/MessageContract.sol
+++ contracts/MessageContract.sol
@@ -3,3 +3,25 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+contract MessageContract is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("PaperTrails", "PT") {
+
+    }
+    function sendMessage(address to, string memory metadata ) public returns (uint256) {
+        
+        _tokenIds.increment();
+        
+        uint256 newMessageId = _tokenIds.current();
+        
+        _mint(to, newMessageId);
+        
+        _setTokenURI(newMessageId, metadata);
+        
+        return newMessageId;
+    }
+}
