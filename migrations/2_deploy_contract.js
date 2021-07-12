7ff4dccece602c0a7fd2996437f8536d58c9a0a3
tsamantanis <philip.tsamantanis@icloud.com>
2021-07-12 09:37:50 -0700

    Deploy migration and update readme to include contract address

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,5 @@
 const MessageContract = artifacts.require("MessageContract");
 
 module.exports = function(deployer){
-    deployer.deploy(MessageContractq);
+    deployer.deploy(MessageContract);
 }
