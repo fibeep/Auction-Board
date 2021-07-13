63d37cfb1a8c069383a618e8dbdd94ced7bb9ebc
tsamantanis <philip.tsamantanis@icloud.com>
2021-07-13 12:55:08 -0700

    Recreate all files from original

--- app/src/index.js
+++ app/src/index.js
@@ -1,76 +1,116 @@
 import Web3 from "web3";
-import metaCoinArtifact from "../../build/contracts/MetaCoin.json";
+import messageArtifact from "../../build/contracts/MessageContract.json";
+import fleek from "@fleekhq/fleek-storage-js";
 
+// Credit: https://github.com/truffle-box/webpack-box/blob/master/app/src/index.js
 const App = {
-  web3: null,
+  
+    web3: null,
   account: null,
-  meta: null,
+  messageContract: null,
 
-  start: async function() {
+  start: async function () {
+    // Connect to Web3 instance.
     const { web3 } = this;
 
     try {
-      // get contract instance
+      // Get contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = metaCoinArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        metaCoinArtifact.abi,
-        deployedNetwork.address,
+      const deployedNetwork = messageArtifact.networks[networkId];
+      this.messageContract = new web3.eth.Contract(
+        messageArtifact.abi,
+        deployedNetwork.address
       );
 
-      // get accounts
+      // Get accounts and refresh the balance.
       const accounts = await web3.eth.getAccounts();
       this.account = accounts[0];
-
       this.refreshBalance();
     } catch (error) {
-      console.error("Could not connect to contract or chain.");
+      console.error("Could not connect to contract or chain: ", error);
     }
   },
 
-  refreshBalance: async function() {
-    const { getBalance } = this.meta.methods;
-    const balance = await getBalance(this.account).call();
+  storeMetadata: async function (to, message) {
+    // Build the metadata.
+    const fromAddress = 0x00
+    var metadata = {
+      name: "Paper Trail",
+      description: `message from ${fromAddress}`,
+      to: to,
+      message: message,
+      timestamp: new Date().toISOString(),
+    };
 
-    const balanceElement = document.getElementsByClassName("balance")[0];
-    balanceElement.innerHTML = balance;
+    // Configure the uploader.
+    const uploadMetadata = {
+      apiKey: "oiTggCSpEQwIs0b+YM6prQ==",
+      apiSecret: "YO0RLnKxpOb6lsE1j1a6ALgXdmflPXLqhj2NnVYlDIM=",
+      key: `metadata/${metadata.timestamp}.json`,
+      data: JSON.stringify(metadata),
+    };
+
+    // Tell the user message is sending.
+    this.setStatus("Sending Message... please wait!");
+
+    // Add the metadata to IPFS first, because our contract requires a
+    // valid URL for the metadata address.
+    const result = await fleek.upload(uploadMetadata);
+
+    // Once the file is added, then we can send a message!
+    this.sendMessage(to, result.publicUrl);
   },
 
-  sendCoin: async function() {
-    const amount = parseInt(document.getElementById("amount").value);
-    const receiver = document.getElementById("receiver").value;
+  sendMessage: async function (to, metadataURL) {
+    // Fetch the sendMessage method from our contract.
+    const { sendMessage } = this.messageContract.methods;
 
-    this.setStatus("Initiating transaction... (please wait)");
+    // Award the message.
+    await sendMessage(to, metadataURL).send({ from: this.account });
 
-    const { sendCoin } = this.meta.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
-
-    this.setStatus("Transaction complete!");
-    this.refreshBalance();
+    // Set the status and show the metadata link on IPFS.
+    this.setStatus(
+      `message sent! View the metadata <a href="${metadataURL}" target="_blank">here</a>.`
+    );
   },
 
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
+  setStatus: function (message) {
+    $("#status").html(message);
   },
 };
 
 window.App = App;
 
-window.addEventListener("load", function() {
+// When all the HTML is loaded, run the code in the callback below.
+$(document).ready(function () {
+  console.log("hello")
+  // Detect Web3 provider.
   if (window.ethereum) {
     // use MetaMask's provider
     App.web3 = new Web3(window.ethereum);
     window.ethereum.enable(); // get permission to access accounts
   } else {
     console.warn(
-      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
+      "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live"
     );
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     App.web3 = new Web3(
-      new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
+      new Web3.providers.HttpProvider("http://127.0.0.1:8545")
     );
   }
+  // Initialize Web3 connection.
+  window.App.start();
 
-  App.start();
+  // Capture the form submission event when it occurs.
+  $("#message-form").submit(function (e) {
+    // Run the code below instead of performing the default form submission action.
+    e.preventDefault();
+
+    // Capture form data and create metadata from the submission.
+    // const name = $("#from").val();
+    const to = $("#to").val();
+    const message = $("#message").val();
+
+    window.App.storeMetadata(to, message);
+  });
 });
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -9,7 +9,26 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+    new CopyWebpackPlugin([
+      {
+         from: "./src/index.html", to: "index.html" 
+      },
+      {
+        from: "./src/search.html", to: "search.html"
+      },
+      {
+          from: "./src/img/pteth.png", to: "pteth.png"
+      },
+      {
+          from: "./src/styles.css", to: "styles.css"
+      },
+      {
+          from: "./src/img/background.png", to: "background.png"
+      },
+      {
+          from: "./src/img/Logo.svg", to: "Logo.svg"
+      },
+    ]),
   ],
   devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
 };
